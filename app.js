0cced9db74c9c29a305849ef27a2bb59e8826452
Fred <fredk@alumni.usc.edu>
2018-10-23 14:09:57 -0700

    first commit

--- app.js
+++ app.js
@@ -1,48 +1,32 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+   const addToDoForm = document.getElementById('addToDoForm');
+   const newToDoText = document.getElementById('newToDoText');
+   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
+   addToDoForm.addEventListener('submit', event => {
+     event.preventDefault();
 
-    //get the text
-    let title = newToDoText.value;
-    //create new li
-    let newLi = document.createElement('li');
-    //create a new input
-    let checkbox = document.createElement('input');
-    //set the input's type to checkbox
-    checkbox.type = "checkbox";
-    //add checkbox class
-    checkbox.classList.add('checkbox','mdl-checkbox__input');
-    //set the title
-    newLi.textContent = title;
-    //add li class
-    newLi.classList.add('mdl-list__item');
-    //append checkbox to newLi
-    newLi.appendChild(checkbox);
+     //get the text
+     let title = newToDoText.value;
+     //create new li
+     let newLi = document.createElement('li');
+     //create a new input
+     let checkbox = document.createElement('input');
+     //set the input's type to checkbox
+     checkbox.type = "checkbox";
+     //set the title
+     newLi.textContent = title;
+     //attach checkbox to the li
+     newLi.appendChild(checkbox);
+     //attach li to ul
+     toDoList.appendChild(newLi);
 
-    //create delete button
-    let delButton = document.createElement('button');
-    delButton.innerHTML = "Delete";
-    //add button class
-    delButton.classList.add('del-but','mdl-button', 'mdl-js-button', 'mdl-button--raised', 'mdl-js-ripple-effect');
-    //append button to newLi
-    newLi.appendChild(delButton);
+     newToDoText.value = '';
 
-    //attach li to ul
-    toDoList.appendChild(newLi);
-    newToDoText.value = '';
+   });
+ };
+ 
 
-    //delete parent element (newLi)
-    delButton.addEventListener ('click', event => {
-      delButton.parentNode.remove();
-    });
-
-  });
-};
-
-window.onload = function() {
-  onReady();
-};
+ window.onload = function() {
+   onReady();
+ };
